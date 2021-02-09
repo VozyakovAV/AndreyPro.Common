46a9cf36e75f11c5efacb77e76b13743474fc2eb
Andrew <andvoz@mail.ru>
2021-02-09 16:37:40 +0400

    MemoryCache2.cs

---
+++ Common/MemoryCache2.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Specialized;
+using System.Runtime.Caching;
+
+namespace AndreyPro.Common
+{
+    public class MemoryCache2 : MemoryCache
+    {
+        private static Lazy<MemoryCache2> _instance = new Lazy<MemoryCache2>();
+        public static MemoryCache2 Instance => _instance.Value;
+
+        private ConcurrentDictionary<string, object> _locks = new ConcurrentDictionary<string, object>();
+
+        public MemoryCache2()
+            : base("MemoryCache2")
+        {
+
+        }
+
+        public MemoryCache2(string name, NameValueCollection config = null)
+            : base(name, config)
+        {
+
+        }
+
+        /// <summary>
+        /// Получить значение из кеша по ключу key
+        /// Если его нет, то вызывается функция func, записывается в кеш значение, и возвращается
+        /// Обеспечивает lock по ключу
+        /// </summary>
+        public object GetOrAddConcurrent(string key, Func<object> func)
+        {
+            return GetOrAddConcurrentImpl(key, func, (c, v) => Set(key, v, new CacheItemPolicy()));
+        }
+
+        /// <summary>
+        /// Получить значение из кеша по ключу key
+        /// Если его нет, то вызывается функция func, записывается в кеш значение, и возвращается
+        /// Обеспечивает lock по ключу
+        /// <param name="timeExpiration"></param>
+        /// </summary>
+        public object GetOrAddConcurrent(string key, int timeExpirationMs, Func<object> func)
+        {
+            return GetOrAddConcurrentImpl(key, func, (c, v) => Set(key, v, DateTime.Now.AddMilliseconds(timeExpirationMs)));
+        }
+
+        private object GetOrAddConcurrentImpl(string key, Func<object> func, Action<MemoryCache, object> actionSetCache)
+        {
+            var value = Get(key);
+            if (value == null)
+            {
+                var locker = _locks.GetOrAdd(key, k => new object());
+                lock (locker)
+                {
+                    value = Get(key);
+                    if (value == null)
+                    {
+                        value = func();
+                        actionSetCache(this, value);
+                    }
+                }
+            }
+            return value;
+        }
+    }
+}
--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using AndreyPro.Common;
+using System;
 using System.Threading.Tasks;
 
 namespace TestConsole
@@ -10,6 +8,25 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
+            Run();
+            Run();
+            
+            Task.Run(Run);
+            Task.Run(Run);
+            Console.ReadKey();
+        }
+
+        static void Run()
+        {
+            var key = "1";
+            var t = MemoryCache2.Instance.GetOrAddConcurrent(key, () =>
+            {
+                //Thread.Sleep(10000);
+                return new object();
+            });
+            var t1 = MemoryCache2.Instance.Get(key);
+            var t2 = MemoryCache2.Instance.Get(key, "1");
+            var t3 = MemoryCache2.Instance.Get(key, "2");
         }
     }
 }
